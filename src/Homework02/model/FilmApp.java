c43a5a4c41a7c1e93fb6718061203f598db279f6
Mesih Alan <nmesihalan@gmail.com>
2023-07-11 00:47:13 +0300

    Bazı düzenlemeler yapıldı

--- src/Homework02/model/FilmApp.java
+++ src/Homework02/model/FilmApp.java
@@ -14,26 +14,21 @@ public class FilmApp {
 
 
     public static void main(String[] args) {
-        /*
-    hangi platformda hangi film var onu görmek istiyorum
-    veritabanı var. ordaki filmleri kaydetsin. category,
-    her kategoride kaç film var görmek istiyorum(Count ile güncellicez)
-    program çalışınca admin misin client mısın onu sor. (1-2 diye basarak)
-    admin iken 0' a basınca client moduna geçsin.
-    category seçmek için scanner'dan 1-2-3 e basarak seçsin.
-    kullanıcı kategorileri görmeli, kategoriye tıklayınca ordaki filmleri(ad, yönetmen, platform, gösterim saati) görmeli
 
- */
-
+      /*
+        Deneme yaparken kullanmak için bazı kategori verileri:
         categoryList.add("Korku");
         categoryList.add("Komedi");
-        categoryList.add("Bilim Kurgu");
-        categoryList.add("Romantik");
-        categoryList.add("Aksiyon");
+        categoryList.add("Dram");
+       */
+
+        //Giriş ekranı gösteriliyor
 
         System.out.println("Film programına hos geldiniz." +
                 "\n Giriş için tuşlama yapınız(Admin için 1'e client için 2'ye basınız)");
 
+
+        // kullanıcıdan alınan inputa göre program ilgili metottan devam edecek.
         int enter = input.nextInt();
         if (enter == 1) {
             adminMode();
@@ -45,8 +40,11 @@ public class FilmApp {
     }
 
     private static void adminMode() {
+
+        //Alınan inputa göre film ya da kategori ekleme ekranına gidecek.
         System.out.println("Hoş geldiniz! Film kaydetmek için 1'e kategori kaydetmek için 2'ye basınız");
         int number = input.nextInt();
+
         while(true) {
             if (number == 1) {
                 addFilm();
@@ -57,20 +55,26 @@ public class FilmApp {
 
             System.out.println("Film eklemek için 1'e categori eklemek için 2'ye çıkış yapmak için 0'a basınız");
             number = input.nextInt();
-            if(number == 0){
+            if(number == 0) {
                 break;
-            }else {
-                continue;
             }
         }
         System.out.println("Client moda geçmek istiyorsanız 0'a basınız");
         int a = input.nextInt();
         if(a==0){
             clientMode();
+            System.out.println("Başka sorgulama yapmak istiyorsanız 0'a basınız.");
+            int i = input.nextInt();
+            if(i == 0){
+                clientMode();
+            }
         }
     }
 
     private static void addFilm() {
+
+        // Kullanıcıdan alınan veriler ile film oluşturuluyor.
+
         input.nextLine();
         System.out.println("Filmin adını giriniz");
         String name = input.nextLine();
@@ -86,13 +90,19 @@ public class FilmApp {
         System.out.println("Filmin imdb puanını giriniz");
         double imdb = input.nextDouble();
 
+        System.out.println("Kategori sayısını giriniz");
+        int a = input.nextInt();
+
         System.out.println("Kategoriyi giriniz");
         List<Category> categoryArr = new ArrayList<>();
 
         for (int i=0;i<categoryList.size();i++){
             System.out.println((i+1)+"-"+categoryList.get(i));
         }
-        int catNum = input.nextInt();
+        int[] catNum = new int[a];
+        for (int i = 0; i < a; i++) {
+               catNum[i] = input.nextInt();
+        }
 
         System.out.println("Filmin süresini giriniz");
         int time = input.nextInt();
@@ -117,50 +127,59 @@ public class FilmApp {
             sessions.add(session);
 
         }
-
+        // Kullanıcıdan alınan veriler ile film objesi oluşurulur.
         Film film = new Film(name, year, director, imdb, time, categoryArr, platformList, sessions);
 
-        Category tempCat = new Category();
-        tempCat.setName(categoryList.get(catNum-1));
+        // Kullanıcıdan alınan kategori bilgilerine göre kategori objesi oluşturularak filme o kategoriler tanımlanır.
         ArrayList <Category> tempCatList = new ArrayList<>();
-        tempCatList.add(tempCat);
-        film.setCategoryArr(tempCatList);
 
+        for (int i = 0; i < catNum.length; i++) {
+            Category tempCat = new Category();
+            tempCat.setName(categoryList.get(catNum[i]-1));
+            tempCatList.add(tempCat);
+        }
+        film.setCategoryArr(tempCatList);
+         // oluşturulan film, film listesine eklenir.
         films.add(film);
         System.out.println("Filminiz kaydedildi!");
 
     }
 
     private static void addCategory() {
+
+        // Kullanıcıdan alınan input ile yeni kategori eklenir.
         input.nextLine();
         System.out.println("Eklemek istediğiniz kategoriyi giriniz");
         String categoryName = input.nextLine();
-
         categoryList.add(categoryName);
         System.out.println("Kategori ekleme işlemi başarılı !\n");
     }
 
 
     private static void clientMode() {
+
         System.out.println("Filmleri Görmek İçin Kategori Seçiniz.");
         for (int i=0;i<categoryList.size();i++){
             System.out.println((i+1)+"-"+categoryList.get(i));
         }
         int userChoice = input.nextInt();
 
+        // Girilen kategoriye ilişkin filmler foreach döngüsü ile görüntüleniyor.
+
         System.out.println(categoryList.get(userChoice-1)+" kategorisindeki filmler :");
 
         int count = 0;
 
-        for (Film fd : films){
-            for (Category c : fd.getCategoryArr()){
+        for (Film f : films){
+            for (Category c : f.getCategoryArr()){
                 if (c.getName().equals(categoryList.get(userChoice-1))){
-                    System.out.println(fd);
+                    System.out.println(f);
                     count++;
                 }
             }
         }
 
-        System.out.println(categoryList.get(userChoice-1)+" kategorisinde "+count+" tane film vardır...");
+        // tanımladığımız count değişkeni sayesinde döngünün dönme sayısına göre kategorideki film adedini tespit edip yazdırıyoruz.
+        System.out.println(categoryList.get(userChoice-1)+" kategorisinde "+count+" tane film vardır.");
     }
 }
